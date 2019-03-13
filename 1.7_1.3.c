629dbed42a2691422b49a4ddc3225ffc71ca9a96
hv90 <hugomachado182@gmail.com>
2019-03-13 01:42:00 -0300

    Unidade 1.7 exercício 1.3

---
+++ 1.7_1.3.c
@@ -0,0 +1,30 @@
+/*
+Desenvolver algoritmo não recursivo para o cálculo do fatorial
+do inteiro n >= 0, de tal forma que prescinda de armazenamento
+de qualquer vetor.
+*/
+
+
+#include <stdio.h>
+
+int fat(int n){
+    int res = 1;
+    
+    for(int i = 1; i <= n; i ++){
+        res *= i;
+    }
+    return res;
+}
+
+int main (int argc, char *argv[]){
+    int n = 5;
+
+    printf("\n\n\nEste programa calcula o fatorial de um número\n\n\n"
+            "Entre um número para calcular o fatorial: \n");
+    
+    scanf("%d", &n);
+    printf("O fatorial eh: %d\n",fat(n));
+
+    return 0;
+}
+
